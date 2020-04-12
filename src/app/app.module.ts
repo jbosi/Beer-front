84b8cf55d294d54db4d690f966cf00490a69ff00
bosi jonathan <jbosi@lucca.fr>
2020-04-12 13:49:43 +0200

    Add: Custom theme, map and first button added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
+import { ButtonFilterComponent } from './components/button-filter/button-filter.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule, MatButtonModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    ButtonFilterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
