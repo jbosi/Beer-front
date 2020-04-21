141ca01b15c69a2b26caf9b7351380b0addbc1b8
jbosi <jonathan.bosi@lilo.org>
2020-04-22 01:28:02 +0200

    deploy environment var fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  mapbox: {
+	accessToken: 'pk.eyJ1IjoiamJvc2kiLCJhIjoiY2s5NzNrN2JpMGVlNTNmdGNtMWZmMWswdyJ9.iyYDNUbxnLkgY49e6N8Z4w'
+  }
+}
