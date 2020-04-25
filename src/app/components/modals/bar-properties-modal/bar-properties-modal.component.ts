eab67aaa9dc4c11b3f45e6f89b6fd88d504f7013
jbosi <jonathan.bosi@lilo.org>
2020-04-25 23:04:11 +0200

    bar properties get real data

--- src/app/components/modals/bar-properties-modal/bar-properties-modal.component.ts
+++ src/app/components/modals/bar-properties-modal/bar-properties-modal.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
 
 //TODO: remove when image will be provided in data
 declare var require: any;
@@ -12,16 +11,10 @@ declare var require: any;
 export class BarPropertiesModalComponent implements OnInit {
 	
 	constructor(
-		public dialogRef: MatDialogRef<BarPropertiesModalComponent>,
-		@Inject(MAT_DIALOG_DATA) public data: any
 	) { }
 
 	ngOnInit() {
-		console.log('data', this.data)
-	}
-
-	closeDialog() {
-		this.dialogRef.close('test!');
+		// console.log('data', this.data)
 	}
 
 	//TODO: remove when image will be provided in data
--- src/app/services/bar-properties.service.ts
+++ src/app/services/bar-properties.service.ts
@@ -4,103 +4,144 @@ import { of, Observable } from 'rxjs';
 import { GeoJson } from '../components/class/map-class';
 import { environment } from 'src/environments/environment';
 import * as mapboxgl from 'mapbox-gl';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class BarPropertiesService {
 	
-	constructor() { 
+	constructor(
+		public http: HttpClient
+	) { 
 		mapboxgl.accessToken = environment.mapbox.accessToken
 	}
 
-	getMarkers(): Observable<any[]> {
+	getMarkers(): Observable<any> {
+		return this.http.get('https://aleforall.herokuapp.com/bars').pipe(
+			map((response: any) => {
+				return response.map( item => {
+
+					const coordinates = item.coordinates.split(',')
+					return {
+						"type": "Feature",
+							"geometry": {
+								"type": "Point",
+								"coordinates": coordinates.reverse()
+							},
+							"properties": {
+								"id":  2,
+								"name": item.name,
+								"address": 'Rue du bar ' + item.name,
+								"beers": [
+									{
+										"id": 6,
+										"pricing": [
+											{
+												"quantity": 50,
+												"price": 5,
+											}
+										],
+										"name": 'straffe hendrik quadruple',
+										"image": '../../icons/beers/beer-brown.png',
+										"degré": 12,
+										"type": 'brown',
+									}
+								],
+							}
+						}
+					}
+				)
+			})
+		)
+
 		// Mock Data
-		return of([{
-			"type": "Feature",
-      		"geometry": {
-				"type": "Point",
-				"coordinates": [
-					2.354605,
-					48.863869
-				]
-			},
-			"properties": {
-				"id": 1,
-				"name": 'Loustic',
-				"address": '40 Rue Chapon, 75003 Paris',
-				"beers": [
-					{
-						"id": 3,
-						"pricing": [
-							{
-								"quantity": 33,
-								"price": 2,
-							}
-						],
-						"name": 'Triple Karmeliet',
-						"degré": 8,
-						"image": '../../icons/beers/beer-blond.png',
-						"type": 'blond',
-					},
-					{
-						"id": 6,
-						"pricing": [
-							{
-								"quantity": 50,
-								"price": 5,
-							}
-						],
-						"name": 'straffe hendrik quadruple',
-						"degré": 12,
-						"image": '../../icons/beers/beer-brown.png',
-						"type": 'brown',
-					},
-					{
-						"id": 8,
-						"pricing": [
-							{
-								"quantity": 33,
-								"price": 4,
-							}
-						],
-						"name": 'Celis white',
-						"degré": 6,
-						"image": '../../icons/beers/beer-white.png',
-						"type": 'white',
-					}
-				],
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					2.35612,
-					48.855793
-				]
-			},
-			"properties": {
-				// "image-name": 'test',
-				// "icon": "https://upload.wikimedia.org/wikipedia/commons/8/88/Map_marker.svg",
-				"id":  2,
-				"name": 'La Perla Bar',
-				"address": '26 Rue François Miron, 75004 Paris',
-				"beers": [
-					{
-						"id": 6,
-						"pricing": [
-							{
-								"quantity": 50,
-								"price": 5,
-							}
-						],
-						"name": 'straffe hendrik quadruple',
-						"image": '../../icons/beers/beer-brown.png',
-						"degré": 12,
-						"type": 'brown',
-					}
-				],
-			}
-		}])
+		// return of([{
+		// 	"type": "Feature",
+      	// 	"geometry": {
+		// 		"type": "Point",
+		// 		"coordinates": [
+		// 			2.354605,
+		// 			48.863869
+		// 		]
+		// 	},
+		// 	"properties": {
+		// 		"id": 1,
+		// 		"name": 'Loustic',
+		// 		"address": '40 Rue Chapon, 75003 Paris',
+		// 		"beers": [
+		// 			{
+		// 				"id": 3,
+		// 				"pricing": [
+		// 					{
+		// 						"quantity": 33,
+		// 						"price": 2,
+		// 					}
+		// 				],
+		// 				"name": 'Triple Karmeliet',
+		// 				"degré": 8,
+		// 				"image": '../../icons/beers/beer-blond.png',
+		// 				"type": 'blond',
+		// 			},
+		// 			{
+		// 				"id": 6,
+		// 				"pricing": [
+		// 					{
+		// 						"quantity": 50,
+		// 						"price": 5,
+		// 					}
+		// 				],
+		// 				"name": 'straffe hendrik quadruple',
+		// 				"degré": 12,
+		// 				"image": '../../icons/beers/beer-brown.png',
+		// 				"type": 'brown',
+		// 			},
+		// 			{
+		// 				"id": 8,
+		// 				"pricing": [
+		// 					{
+		// 						"quantity": 33,
+		// 						"price": 4,
+		// 					}
+		// 				],
+		// 				"name": 'Celis white',
+		// 				"degré": 6,
+		// 				"image": '../../icons/beers/beer-white.png',
+		// 				"type": 'white',
+		// 			}
+		// 		],
+		// 	}
+		// },
+		// {
+		// 	"type": "Feature",
+		// 	"geometry": {
+		// 		"type": "Point",
+		// 		"coordinates": [
+		// 			2.35612,
+		// 			48.855793
+		// 		]
+		// 	},
+		// 	"properties": {
+		// 		// "image-name": 'test',
+		// 		// "icon": "https://upload.wikimedia.org/wikipedia/commons/8/88/Map_marker.svg",
+		// 		"id":  2,
+		// 		"name": 'La Perla Bar',
+		// 		"address": '26 Rue François Miron, 75004 Paris',
+		// 		"beers": [
+		// 			{
+		// 				"id": 6,
+		// 				"pricing": [
+		// 					{
+		// 						"quantity": 50,
+		// 						"price": 5,
+		// 					}
+		// 				],
+		// 				"name": 'straffe hendrik quadruple',
+		// 				"image": '../../icons/beers/beer-brown.png',
+		// 				"degré": 12,
+		// 				"type": 'brown',
+		// 			}
+		// 		],
+		// 	}
+		// }])
 	}
 }
