621a4f0d42980728879488a1b25fe904011e131b
Jonathan B <47146539+jbosi@users.noreply.github.com>
2020-05-03 21:40:39 +0200

    Add: draft home page and popup (#5)
    * Theme & home page added, router & service modified
    
    * Add: home button link and custom svg marker

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -7,3 +7,4 @@ export * from './modals';
 export * from './nav-bar';
 export * from './page-not-found';
 export * from './search-auto-complete';
+export * from './home';
--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PageNotFoundComponent, AddBeerFormComponent, MapPageComponent } from './components';
+import { PageNotFoundComponent, AddBeerFormComponent, MapPageComponent, HomeComponent } from '../components';
 
 const routes: Routes = [
 	{ path: 'map', component: MapPageComponent },
 	{ path: 'add-beer', component: AddBeerFormComponent },
-	{ path: '', redirectTo: '/map', pathMatch: 'full' },
+	{ path: '', component: HomeComponent, pathMatch: 'full' },
 	{ path: '**', component: PageNotFoundComponent }, 
 ];
 @NgModule({
