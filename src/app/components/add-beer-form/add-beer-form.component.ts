9433673e70d5e5fee26eaaf1a873c0c041f9aa7d
baptistemon <63585162+baptistemon@users.noreply.github.com>
2020-05-05 22:42:25 +0200

    fix indent issues

--- src/app/components/add-beer-form/add-beer-form.component.ts
+++ src/app/components/add-beer-form/add-beer-form.component.ts
@@ -1,94 +1,91 @@
 import { beerInfo } from './../../models/bar-properties.model';
 import { BeerPropertiesService } from './../../services/beer-properties.service';
-
 import { Component, forwardRef, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormBuilder, FormGroup,
-Validators, FormControl, NG_VALIDATORS, Form } from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormBuilder, FormGroup, FormControl, NG_VALIDATORS } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 
 @Component({
-selector: 'app-add-beer-form',
-templateUrl: './add-beer-form.component.html',
-styleUrls: ['./add-beer-form.component.scss'],
-providers: [
-{
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => AddBeerFormComponent),
-  multi: true
-},
-{
-  provide: NG_VALIDATORS,
-  useExisting: forwardRef(() => AddBeerFormComponent),
-  multi: true,
-}
-],
-changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-add-beer-form',
+	templateUrl: './add-beer-form.component.html',
+	styleUrls: ['./add-beer-form.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => AddBeerFormComponent),
+			multi: true
+		},
+		{
+			provide: NG_VALIDATORS,
+			useExisting: forwardRef(() => AddBeerFormComponent),
+			multi: true,
+		}
+	],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class AddBeerFormComponent implements ControlValueAccessor, OnDestroy {
-  private options: string[] = ['blonde', 'brune', 'noire', 'ambree', 'rouge', 'fruitee'];
-  form: FormGroup;
-  subscriptions: Subscription[] = [];
-  onChange: any = () => {};
-  onTouched: any = () => {};
-
-  get value(): beerInfo {
-    return this.form.value;
-  }
-
-  set value(value: beerInfo) {
-    this.form.setValue(value);
-    this.onChange(value);
-    this.onTouched();
-  }
-
-  get nameControl() {
-    return this.form.controls.name;
-  }
-
-  constructor( private beerPropertiesService: BeerPropertiesService, private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({
-      name: [],
-      brewery: [],
-      degre: [],
-      type: []
-      });
-
-    this.subscriptions.push(
-      this.form.valueChanges.subscribe(value => {
-        this.onChange(value);
-        this.onTouched();
-      })
-    );
-  }
-
-
-  ngOnDestroy() {
-    this.subscriptions.forEach(s => s.unsubscribe());
-  }
-
-  registerOnChange(fn) {
-    this.onChange = fn;
-  }
-
-  writeValue(value) {
-    if (value) {
-      this.value = value;
-    }
-
-    if (value == null) {
-      this.form.reset();
-    }
-  }
-
-  registerOnTouched(fn) {
-    this.onTouched = fn;
-  }
-
-  validate(_: FormControl) {
-    return this.form.valid ? null : { addBeer: { valid: false, }, };
-  }
-
-
+	public options: string[] = ['blonde', 'brune', 'noire', 'ambree', 'rouge', 'fruitee'];
+	public form: FormGroup;
+	private subscriptions: Subscription[] = [];
+	onChange: any = () => {};
+	onTouched: any = () => {};
+	
+	get value(): beerInfo {
+		return this.form.value;
+	}
+	
+	set value(value: beerInfo) {
+		this.form.setValue(value);
+		this.onChange(value);
+		this.onTouched();
+	}
+	
+	get nameControl() {
+		return this.form.controls.name;
+	}
+	
+	constructor( private beerPropertiesService: BeerPropertiesService, private formBuilder: FormBuilder) {
+		this.form = this.formBuilder.group({
+			name: [],
+			brewery: [],
+			degre: [],
+			type: []
+		});
+		
+		this.subscriptions.push(
+			this.form.valueChanges.subscribe(value => {
+				this.onChange(value);
+				this.onTouched();
+			})
+		);
+	}
+		
+		
+	ngOnDestroy() {
+		this.subscriptions.forEach(s => s.unsubscribe());
+	}
+	
+	registerOnChange(fn) {
+		this.onChange = fn;
+	}
+	
+	writeValue(value) {
+		if (value) {
+			this.value = value;
+		}
+		
+		if (value == null) {
+			this.form.reset();
+		}
+	}
+	
+	registerOnTouched(fn) {
+		this.onTouched = fn;
+	}
+	
+	validate(_: FormControl) {
+		return this.form.valid ? null : { addBeer: { valid: false } };
+	}
+		
 }
