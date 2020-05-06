ba6cba36db82ae47650c35dcbf9edaf19de115f9
jbosi <jonathan.bosi@lilo.org>
2020-05-06 02:03:04 +0200

    Css

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PageNotFoundComponent, AddBeerFormComponent, MapPageComponent, HomeComponent } from '../components';
+import { PageNotFoundComponent, AddBeerFormComponent, MapPageComponent, HomeComponent, NavBarComponent } from '../components';
 
 const routes: Routes = [
-	{ path: 'map', component: MapPageComponent },
-	{ path: 'add-beer', component: AddBeerFormComponent },
-	{ path: '', component: HomeComponent, pathMatch: 'full' },
+	{
+		path: '', component: HomeComponent,	
+		pathMatch: 'full',
+	},
+	{
+		path: '',
+		component: NavBarComponent,
+		children: [
+			{ path: 'map', component: MapPageComponent },
+			{ path: 'add-beer', component: AddBeerFormComponent },
+		]
+	},
 	{ path: '**', component: PageNotFoundComponent }, 
 ];
 @NgModule({
