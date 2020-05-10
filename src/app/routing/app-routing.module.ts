56de1a7fbba8a160c1823f0057914625e0b21e2b
Jonathan B <47146539+jbosi@users.noreply.github.com>
2020-05-10 20:13:18 +0200

    Router, map, clusters and landing page updates (#8)
    * Css
    
    * Add beer styles removed
    
    * Clusters & map component updated

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PageNotFoundComponent, MapPageComponent, HomeComponent, AddBeerPageComponent } from '../components';
+import { PageNotFoundComponent, AddBeerPageComponent, MapPageComponent, HomeComponent, NavBarComponent } from '../components';
 
 const routes: Routes = [
-	{ path: 'map', component: MapPageComponent },
-	{ path: 'add-beer', component: AddBeerPageComponent },
-	{ path: '', component: HomeComponent, pathMatch: 'full' },
+	{
+		path: '', component: HomeComponent,	
+		pathMatch: 'full',
+	},
+	{
+		path: '',
+		component: NavBarComponent,
+		children: [
+			{ path: 'map', component: MapPageComponent },
+			{ path: 'add-beer', component: AddBeerPageComponent },
+		]
+	},
 	{ path: '**', component: PageNotFoundComponent }, 
 ];
 @NgModule({
