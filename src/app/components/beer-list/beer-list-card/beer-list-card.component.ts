b71e5cb2cdb93e58ccdc56f01bd7b42a0f394457
jbosi <jonathan.bosi@lilo.org>
2020-06-10 12:56:23 +0200

    Add: Card list

--- src/app/components/beer-list/beer-list-card/beer-list-card.component.ts
+++ src/app/components/beer-list/beer-list-card/beer-list-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+declare var require: any;
 
 @Component({
 	selector: 'app-beer-list-card',
 	templateUrl: './beer-list-card.component.html',
 	styleUrls: ['./beer-list-card.component.scss']
 })
+
 export class BeerListCardComponent implements OnInit {
 	public beerIcon = require('./../../../../icons/beers/beerIcon.svg');
 
