d71f205a7e3d32e5a4107b9e71ff6adfaa3e1661
Jonathan B <47146539+jbosi@users.noreply.github.com>
2020-08-21 14:47:01 +0200

    Mod: Replaced material tables by cutom tables in most of the app (#34)
    * Add: beer table & admin table components
    
    * Style improved
    
    * Add: styles for buttons

--- src/app/components/admin-dashboard/user-admin/user-admin.component.ts
+++ src/app/components/admin-dashboard/user-admin/user-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/users.service';
-import { zip, throwError, Observable } from 'rxjs';
+import { zip } from 'rxjs';
+import { IcolumnPropertiesInterface } from '../../shared';
 
 @Component({
 	selector: 'app-user-admin',
@@ -14,14 +15,14 @@ export class UserAdminComponent implements OnInit {
 	public userData: any;
 	public userOwners: any;
 	public userOwnersRequests: any;
-	public userColumns: IcolumnsInterface;
-	public userOwnersColumns: IcolumnsInterface;
-	public userOwnersRequestsColumns: IcolumnsInterface;
+	public userColumns: IcolumnPropertiesInterface[];
+	public userOwnersColumns: IcolumnPropertiesInterface[];
+	public userOwnersRequestsColumns: IcolumnPropertiesInterface[];
 
 	constructor(
 		private readonly userService: UserService
 	) { }
-	
+
 	ngOnInit() {
 		zip(
 			this.userService.getAllUsers(),
@@ -29,7 +30,7 @@ export class UserAdminComponent implements OnInit {
 			this.userService.getAllOwnershipRequests()
 		).subscribe(([users, owners, ownersRequests]) => {
 			this.userList = users;
-			
+
 			this.ownersList = owners
 				.filter(owner => owner.user != null && owner.bar != null)
 				.map(owner => {
@@ -57,121 +58,88 @@ export class UserAdminComponent implements OnInit {
 		this.userOwnersRequestsColumns = this.initUserOwnersRequestsColumns();
 	}
 
-	private initUserColumns(): IcolumnsInterface {
-		return {
-			columnsNames: ['username', 'email', 'isAdmin'],
-			columnsProperties: [
-				{
-					columnName: 'username',
-					value: 'username',
-					title: 'Username',
-				},
-				{
-					columnName: 'email',
-					value: 'email',
-					title: 'Email',
-				},
-				{
-					columnName: 'isAdmin',
-					value: 'isAdmin',
-					title: 'IsAdmin',
-				}
-			]
-		};
+	private initUserColumns(): IcolumnPropertiesInterface[] {
+		return [
+			{
+				columnName: 'username',
+				value: 'username',
+				title: 'Username',
+			},
+			{
+				columnName: 'email',
+				value: 'email',
+				title: 'Email',
+			},
+			{
+				columnName: 'isAdmin',
+				value: 'isAdmin',
+				title: 'IsAdmin',
+			}
+		];
 	}
 
-	private initUserOwnersColumns(): IcolumnsInterface {
-		return {
-			columnsNames: ['name', 'barName', 'email', 'userId'],
-			columnsProperties: [
-				{
-					columnName: 'name',
-					value: 'username',
-					title: 'User Name',
-				},
-				{
-					columnName: 'barName',
-					value: 'barName',
-					title: 'Bar name',
-				},
-				{
-					columnName: 'email',
-					value: 'email',
-					title: 'Email',
-				},
-				{
-					columnName: 'userId',
-					value: 'userId',
-					title: 'User id',
-				},
-			]
-		};
+	private initUserOwnersColumns(): IcolumnPropertiesInterface[] {
+		return [
+			{
+				columnName: 'name',
+				value: 'username',
+				title: 'User Name',
+			},
+			{
+				columnName: 'barName',
+				value: 'barName',
+				title: 'Bar name',
+			},
+			{
+				columnName: 'email',
+				value: 'email',
+				title: 'Email',
+			},
+			{
+				columnName: 'userId',
+				value: 'userId',
+				title: 'User id',
+			},
+		];
 	}
 
-	private initUserOwnersRequestsColumns(): IcolumnsInterface {
-		return {
-			columnsNames: ['barName', 'username', 'reason', 'pictures', 'studied', 'accepted', 'icon'],
-			columnsProperties: [
-				{
-					columnName: 'barName',
-					value: 'barName',
-					title: 'Bar name',
-				},
-				{
-					columnName: 'username',
-					value: 'username',
-					title: 'username',
-				},
-				{
-					columnName: 'reason',
-					value: 'reason',
-					title: 'Reason',
-				},
-				{
-					columnName: 'pictures',
-					value: 'pictures',
-					title: 'Pictures',
-				},
-				{
-					columnName: 'studied',
-					value: 'studied',
-					title: 'Studied',
-				},
-				{
-					columnName: 'accepted',
-					value: 'accepted',
-					title: 'Accepted',
-				},
-				{
-					columnName: 'icon',
-					value: 'icon',
-					title: 'icon',
-				}
-			]
-		};
+	private initUserOwnersRequestsColumns(): IcolumnPropertiesInterface[] {
+		return [
+			{
+				columnName: 'barName',
+				value: 'barName',
+				title: 'Bar name',
+			},
+			{
+				columnName: 'username',
+				value: 'username',
+				title: 'username',
+			},
+			{
+				columnName: 'reason',
+				value: 'reason',
+				title: 'Reason',
+			},
+			{
+				columnName: 'pictures',
+				value: 'pictures',
+				title: 'Pictures',
+			},
+			{
+				columnName: 'studied',
+				value: 'studied',
+				title: 'Studied',
+			},
+			{
+				columnName: 'accepted',
+				value: 'accepted',
+				title: 'Accepted',
+			},
+			{
+				columnName: 'icon',
+				value: 'icon',
+				title: 'icon',
+			}
+		];
 	}
-
-	public onAcceptOrRefuse(element, state: boolean): void | Observable<never> {
-		console.log(element)
-		if (element.user == null || element.bar == null) {
-			return throwError("user or bar undefined");
-		}
-		this.userService.acceptOrRefuseRequest({
-			userId: element.user.id,
-			barId: element.bar.id,
-			stateRequest: state
-		}).subscribe();
-	}
-}
-
-export interface IcolumnPropertiesInterface {
-	columnName: string;
-	value: string;
-	title: string;
-	class?: string;
-}
-
-export interface IcolumnsInterface {
-	columnsNames: string[];
-	columnsProperties: IcolumnPropertiesInterface[];
 }
