a72851dc0f0444c35b4ad155f8c584e403290dca
jbosi <jonathan.bosi@lilo.org>
2021-01-02 02:13:45 +0100

    Fix: beer page

--- src/app/pages/beers/beers.module.ts
+++ src/app/pages/beers/beers.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { BeerListCardComponent } from './components';
 import { RouterModule, Routes } from '@angular/router';
 import { BeersComponent } from './beers.component';
+import { SharedModule } from '@beer/shared';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const routes: Routes = [
 	{
@@ -11,14 +14,20 @@ const routes: Routes = [
 	}
 ];
 
+const MATERIAL_MODULES = [
+	MatIconModule,
+	MatProgressSpinnerModule,
+];
 @NgModule({
 	declarations: [
 		BeersComponent,
 		BeerListCardComponent
 	],
 	imports: [
+		MATERIAL_MODULES,
 		RouterModule.forChild(routes),
 		CommonModule,
+		SharedModule
 	]
 })
 export class BeersModule { }
